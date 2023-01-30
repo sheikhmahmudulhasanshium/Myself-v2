9d3ac7bb282c90fb35f192eb348efb27b0bd335c
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2023-01-30 13:51:32 +0600

    html to php is converted

---
+++ MVC/View/about.php
@@ -0,0 +1,202 @@
+
+<html>
+    <head>
+        <h1 align="center">About</h1>
+        
+    </head>
+    <body bgcolor="#0E6EE8">
+        <center>
+            <img src="F:\Data\OneDrive\Pictures\photo\shium 2.jpg" alt="X" height="160",width="160">
+            </center>
+            
+        <table>
+            <tr>
+                <td ><h2> Personal information</h2>
+                <!--<table hidden> for details https://www.dofactory.com/html/editor/table-hidden-toggle-->
+                      
+                    <table class="about",id="about_me" hidden>
+                    <tr>
+                        <td align="right"><b>Full Name:   </b></td>
+                        <td>Sheikh Mahmudul Hasan Shium</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Father's Name:   </b></td>
+                        <td>Sheikh Akram Uddin</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Mother's Name:   </b></td>
+                        <td>Sahida Sultana Hira</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Gender:   </b></td>
+                        <td>Male</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Blood Group:   </b></td>
+                        <td>A+</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Date of Birth:   </b></td>
+                        <td>24/12/2000</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Phone Number:   </b></td>
+                        <td>+8801729771453</td>
+                        
+                    </tr>
+                    
+                    <tr>
+                        <td align="right"><b>Religion:   </b></td>
+                        <td>Islam</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Marital Status:   </b></td>
+                        <td>Unmarried</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Home District:   </b></td>
+                        <td>Khulna</td>
+                        
+                    </tr>
+                    <tr>
+                        <td align="right"><b>Nationality:   </b></td>
+                        <td>Bangladeshi</td>
+                        
+                    </tr>
+                </table>
+                  
+                </td>
+            </tr>
+            <tr>
+                <td><h2> 
+                        Addresss
+                </h2>
+                     <table hidden>
+                        <tr>
+                            <td align="right"><b> Present Address:  </b></Address></td>
+                            <td>
+                                <b>House No:   </b> 254,
+                                <b>Road No: </b>9,
+                                <b>Block:   </b>C,
+                                Bashundhara R/A,
+                                <b>Division:    </b> Dhaka.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right"><b> Permanent Address: </b></td>
+                            <td>
+                                <b>Holding No:   </b> 82,
+                                <b>Police Station: </b>Aronghata,
+                                <b>Post Office:  </b>BIT-9203 (KUET),
+                                <b>Village: </b>Teligati,
+                                <b>District:    </b>Khulna,
+                                <b>Division:    </b> Khulna.
+                            </td>
+                        </tr>
+                    </table>
+ 
+                    </td>
+            </tr>
+            
+            <tr>
+                <td><h2> Academic Qualification </h2>
+                    <table border="2" hidden>
+                        
+                        <tr>
+                            <td> 
+                                <b>
+                                Passing Year
+                                </b>
+                            </td>
+                            <td><b> Exam</b></td>
+                            <td> <b>
+                                
+                                Institute Name
+                                </b>
+                            </td>
+                            <td><b> Group/Department</b></td>
+                            <td> <b>
+                                Board
+                                </b>
+                            </td>
+                            
+                            <td> <b>
+                                Result (Division/GPA/CGPA)
+                                </b>
+                            </td>
+                        
+                        </tr>
+                        <tr>
+                            <td>2011</td>
+                            <td>P.S.C</td>
+                            <td>Ashuganj Tap Bidyut Kendra High School</td>
+                            <td>Science</td>
+                            <td>Comilla Board</td>
+                            <td>1st Division</td>
+                        </tr>
+
+                        <tr>
+                            <td>2014</td>
+                            <td>J.S.C</td>
+                            <td>Ashuganj Tap Bidyut Kendra High School</td>
+                            <td>Science</td>
+                            <td>Comilla Board</td>
+                            <td>G.P.A-5.00 out of 5.00</td>
+                        </tr>
+                        <tr>
+                            <td>2016</td>
+                            <td>S.S.C</td>
+                            <td>Ashuganj Tap Bidyut Kendra High School</td>
+                            <td>Science</td>
+                            <td>Comilla Board</td>
+                            <td>G.P.A-4.94 out of 5.00</td>
+                        </tr>
+                        
+                            <tr>
+                                <td>2018</td>
+                                <td>H.S.C</td>
+                                <td>Abdul Kadir Mollah City College</td>
+                                <td>Science</td>
+                                <td>Dhaka Board</td>
+                                <td>G.P.A-4.17 out of 5.00</td>
+                            </tr>
+    
+                        <tr>
+                            <td>2023</td>
+                            <td>B.S.C</td>
+                            <td>American International University-Bangladesh</td>
+                            <td>Computer Science And Engineering</td>
+                            <td>&nbsp;</td>
+                            <td>C.G.P.A-3.86 out of 4.00</td>
+                        </tr>
+
+                    </table>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <h2>
+                        Personal Interest
+                    </h2>
+                    <table hidden>
+                        <tr>
+                            <td>
+                                <b>
+                                    Blockchain Technologies, Robotics, IOT, Machine Learning, Artificial Intelligence, Software Quality Assurance & Testing,  etc.
+                                </b>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <?php include 'footer_all.php';?>        
---
+++ MVC/View/footer_all.php
@@ -0,0 +1,19 @@
+<footer align="center">
+            <button type="submit">
+                <a href="homepage.html">
+                    Home
+                </a>
+            </button>
+            <button type="submit"><a href="signin.html">Sign-in </a> </button>
+            <button type="submit"><a href="login.html">Log-in</a></button>
+            <button type="submit"><a href="contact.html">Contact</a> </button>
+            <button type="submit"><a href="about.html"> About </a></button>
+        </footer>
+        
+           
+        
+    </body> 
+     
+</html>
+  
+          
